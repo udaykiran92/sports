442d840a6f3d134f8b890743b695ec2361b1a4f3
DESKTOP-9EPR0KG\Venkat <venkat@nendrasys.com>
2022-08-23 12:08:33 +0530

    change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './components/cart/cart.component';
+// import { CheckoutComponent } from './components/checkout/checkout.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { ProductsComponent } from './components/products/products.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 const routes: Routes = [
   //{path:'',component:HeaderComponent},
   {path:'',redirectTo:'products',pathMatch:'full'},
   {path:'',component:ProductsComponent},
   {path:'bag',component:CartComponent},
-  {path:'Login',component:LoginPageComponent}
+  {path:'Login',component:LoginPageComponent},
+  // {path:'check', component:CheckoutComponent},
+  {path:'sign', component:SignupComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { FilterPipe } from './shared/filter.pipe';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FooterComponent } from './components/footer/footer.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { SignupComponent } from './components/signup/signup.component';
     LoginPageComponent,
     FilterPipe,
     FooterComponent,
-    SignupComponent
+    SignupComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
